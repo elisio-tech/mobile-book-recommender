52e9a28c8f4a7303cfe4cc41ed63226fd57b201d
Elisio Augusto <elisiouiux@gmail.com>
2026-03-30 21:27:16 +0100

    index

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View>
-      <Text>Books recomendations</Text>
+      <Text>Hello world!</Text>
     </View>
   );
 }
