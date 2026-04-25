fb77b63692753be5404c2bf34e620c8dff1d9957
Elisio Augusto <elisiouiux@gmail.com>
2026-04-25 17:47:45 +0100

    start app

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View style={style.conteiner}>
       <Text>Init projects!</Text>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  conteiner: {
+    flex: 1,
+    textAlign: "center",
+  },
+});
